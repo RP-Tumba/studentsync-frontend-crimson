5986d2f64ca7de6477e399a1f9c5ded6187817f4
Your Name <samuelishimw02@gmail.com>
2025-05-14 20:20:38 +0200

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,28 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import StudentList from "./pages/StudentList";
 import "./App.css";
+import { HomePage } from "./pages/Home/Home";
 import { Form } from "./InsertNewStudent/Insert";
 
-
 const App = () => {
+  
   return (
     <Router>
-      {/* <div className="app-container">
-        <Navbar />
+      <div className="app-container">
+      {  <Navbar />}
         <main className="main-content">
-          <Routes>
-            <Route path="/" element={<StudentList />} />
-            <Route path="/" element={<></>} />
+          
+            <Routes>
+            <Route path="/studentsList" element={<StudentList />} />
+            <Route path="/studentsList" element={<Form />} />
+            <Route index element={ <HomePage />} />
+           
           </Routes>
-        </main>
-      </div> */}
-      <Form/>
-    
+          
+        </main> 
+      </div>
     </Router>
-
+    
   );
 };
 
